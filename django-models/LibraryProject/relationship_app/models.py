9dd308a3831c31b46e52ce4833729a5a54056771
Mohamedbbahaa <mohamedbbahaa404@gmail.com>
2024-11-10 22:38:09 +0200

    a

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=50)
-     def __str__(self):
+    def __str__(self):
         return self.name
 
 class Book(models.Model):
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from relationship_app.models import Book, Library
+from django.views.generic import DetailView
 
+def book_list_view(request):
+    books = Book.objects.select_related('author').all()
+    return render(request, 'relationship_app/book_list.html', {'books' : books})
 # Create your views here.
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        # Get the existing context data from DetailView
+        context = super().get_context_data(**kwargs)
+        # Add books related to this library to the context
+        context['books'] = self.object.books.all()
+        return context
