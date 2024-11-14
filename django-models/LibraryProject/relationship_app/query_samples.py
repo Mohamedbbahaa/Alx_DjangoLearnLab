97f5a0949eb8bd3a540e950fb456ee1ae5761851
Mohamedbbahaa <mohamedbbahaa404@gmail.com>
2024-11-14 15:17:09 +0200

    1

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,41 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+# Query all books by a specific author
+def get_books_by_author(author_name):
+    try:
+        author = Author.objects.get(name=author_name)
+        books = Book.objects.filter(author=author)
+        return books
+    except Author.DoesNotExist:
+        return f"No author found with the name '{author_name}'"
+
+# List all books in a library
+def get_books_in_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        books = library.books.all()  # Many-to-Many relation
+        return books
+    except Library.DoesNotExist:
+        return f"No library found with the name '{library_name}'"
+
+# Retrieve the librarian for a library
+def get_librarian_for_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        librarian = library.librarian  # One-to-One relation
+        return librarian
+    except Library.DoesNotExist:
+        return f"No library found with the name '{library_name}'"
+    except Librarian.DoesNotExist:
+        return f"No librarian assigned to the library '{library_name}'"
+
+from relationship_app.query_samples import get_books_by_author, get_books_in_library, get_librarian_for_library
+
+# Query all books by a specific author
+print(get_books_by_author("Author Name"))
+
+# List all books in a library
+print(get_books_in_library("Library Name"))
+
+# Retrieve the librarian for a library
+print(get_librarian_for_library("Library Name"))
