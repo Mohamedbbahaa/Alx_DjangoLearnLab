ec125c33c7dba6fa34711d931192518590bab506
Mohamedbbahaa <mohamedbbahaa404@gmail.com>
2024-11-17 19:30:44 +0200

    login

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -26,6 +26,6 @@ class LibraryDetailView(DetailView):
 
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy("login")
     template_name = "templates/relationship_app/register.html"
