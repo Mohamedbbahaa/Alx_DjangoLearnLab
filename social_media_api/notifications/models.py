8b6a7ad2baf386f55161b3a9587bfc6d39cb4913
Mohamedbbahaa <mohamedbbahaa404@gmail.com>
2024-12-20 21:12:59 +0200

    notifications

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -13,7 +13,7 @@ class Notification(models.Model):
     target_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, related_name='target_type', blank=True, null=True)
     target_object_id = models.PositiveIntegerField(blank=True, null=True)
     target = GenericForeignKey('target_content_type', 'target_object_id')
-    date = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Notficiation for {self.recipient.username} by {self.actor.username}"
